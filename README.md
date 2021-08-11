569e5a7989f2b9e8ea124f68f6813358abfbf4fb
maso0310 <balimon@hotmail.com>
2021-08-11 18:15:53 +0800

    'ok'

---
+++ README.md
@@ -0,0 +1,15 @@
+#這個是基於python程式碼與MongoDB互動的範例，其中包括了：
+###詳細內容請見mongodb_function.py
+
+1.資料庫(Database)的建置
+2.集合(Collection)的建置
+3.文檔(Document)的CRUD(建立、閱讀、修改、刪除)
+↑上面三者的關係為：
+
+MongoClient的資料庫(Database)裡面，可以擁有多個集合(Collection)，相當於關聯式資料庫裡面的資料表(Table)，
+每個集合當中，存在取多筆資料，每一筆資料被稱作為一個文檔(Document)，這是屬於在MongoDB資料庫當中最末端的價值單元
+因此如果我們希望可以進行資料存取操作，首先要先建立一個資料庫與集合，才能把資料寫入其中
+
+client: 一個透過pymongo建立的MongoDB連線
+db: 讀取clinet當中，名為MongoClient的Database
+col: 讀取db當中，名為Database的Collection
--- mongodb_function.py
+++ mongodb_function.py
@@ -1,33 +1,8 @@
-'''
-這個是基於python程式碼與MongoDB互動的範例，其中包括了：
-1.資料庫(Database)的建置
-2.集合(Collection)的建置
-3.文檔(Document)的CRUD(建立、閱讀、修改、刪除)
-↑上面三者的關係為：
-
-MongoClient的資料庫(Database)裡面，可以擁有多個集合(Collection)，相當於關聯式資料庫裡面的資料表(Table)，
-每個集合當中，存在取多筆資料，每一筆資料被稱作為一個文檔(Document)，這是屬於在MongoDB資料庫當中最末端的價值單元
-因此如果我們希望可以進行資料存取操作，首先要先建立一個資料庫與集合，才能把資料寫入其中
-
-client: 一個透過pymongo建立的MongoDB連線
-
-dbnames: 用來列出目前有的database_names
-
-db: 讀取clinet當中，名為MongoClient的Database
-col: 讀取db當中，名為Database的Collection
-
-
-'''
 import pymongo
 
 # 要獲得mongodb網址，請至mongodb網站申請帳號進行資料庫建立，網址　https://www.mongodb.com/
 # 獲取的網址方法之範例如圖： https://i.imgur.com/HLCk99r.png
 client = pymongo.MongoClient("自己的mongodb連線網址")
-
-
-#dblist = client.list_databases
-#dbnames = client.database_names
-
 
 #第一個db的建立
 db = client['MongoClient']
